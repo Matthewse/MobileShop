5d030c7dc313bfd6e96599f8960c45eaea6098ce
Matthew <m-semenets@mail.ru>
2019-12-19 15:59:25 +0300

    Add data.json in CopyWebpackPlugin

--- config/webpack.base.conf.js
+++ config/webpack.base.conf.js
@@ -83,7 +83,8 @@ module.exports = {
       ),
       new CopyWebpackPlugin([
          { from: `${PATHS.src}/${PATHS.assets}img`, to: `${PATHS.assets}img` },
-         { from: `${PATHS.src}/${PATHS.assets}fonts`, to: `${PATHS.assets}fonts` }
+         { from: `${PATHS.src}/${PATHS.assets}fonts`, to: `${PATHS.assets}fonts` },
+         { from: `${PATHS.src}/${PATHS.assets}data`, to: `${PATHS.assets}data` }
       ])
    ]
 }
